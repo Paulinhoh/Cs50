8d53153d7405190b57781c0cb9b7a7f36b12d77d
Paulo Henrique Reis <phdsreis@gmail.com>
2024-08-04 11:09:30 -0300

    feat: iniciando os esrcicios do modulo 1

---
+++ modulo_1/exercicios/01/mario.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int tijolos(int tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        for (int j = 0; j < tam; j++)
+        {
+        }
+    }
+}
+
+int main(void)
+{
+    int tamanho = get_int("Tamanho: ");
+    tijolos(tamanho);
+}
